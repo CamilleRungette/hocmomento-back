76e5d6d6644eebf6deef2da416a9e68b86448dab
CamilleRungette <c.rungette@gmail.com>
2023-03-09 17:41:14 +0100

    remove script

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const partnersRoutes = require("./src/routes/partners");
 
 const express = require("express");
 const app = express();
-const port = process.env.port;
+const port = process.env.port || 3000;
 
 connectDb().catch((e) => console.log(e));
 
